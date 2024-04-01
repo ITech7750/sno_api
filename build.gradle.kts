67f7396aa86b007e7b180893caa54a270e4008d1
Игорь <tigor7750@gmail.com>
2024-04-01 18:25:53 +0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
+    kotlin("plugin.jpa") version "1.9.23"
+}
+
+group = "ru.itech"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Для работы с JPA и JDBC
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    // Для web приложения (Tomcat, Jackson) и для обработки JSON
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    // Безопасность
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+
+
+    //  Базовые пакеты
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    //  Для автоматизации документирования REST API
+    implementation("org.springdoc","springdoc-openapi-starter-webmvc-ui", "2.0.2")
+    // Для работы с БД mysql
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
