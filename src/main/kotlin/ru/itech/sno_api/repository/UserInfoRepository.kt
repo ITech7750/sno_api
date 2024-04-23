7900e1988dfc964ecf2273b464378be4c29085e7
Игорь <tigor7750@gmail.com>
2024-04-24 01:19:20 +0300

    corrected

--- src/main/kotlin/ru/itech/sno_api/repository/UserInfoRepository.kt
+++ src/main/kotlin/ru/itech/sno_api/repository/UserInfoRepository.kt
@@ -2,10 +2,11 @@ package ru.itech.sno_api.repository
 
 import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 import ru.itech.sno_api.entity.UserEntity
 import ru.itech.sno_api.entity.UserInfoEntity
 import java.util.*
-
+@Repository
 interface UserInfoRepository : CrudRepository<UserInfoEntity, Long> {
 
 
