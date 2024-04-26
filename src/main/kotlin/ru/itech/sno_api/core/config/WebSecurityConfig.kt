e11bd1df63a935314dfc46ef0f4cdda9384b7901
Игорь <tigor7750@gmail.com>
2024-04-26 21:09:02 +0300

    with CORS

--- src/main/kotlin/ru/itech/sno_api/core/config/WebSecurityConfig.kt
+++ src/main/kotlin/ru/itech/sno_api/core/config/WebSecurityConfig.kt
@@ -14,6 +14,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.stereotype.Component
 import ru.itech.sno_api.core.AuthEntryPoint
 import ru.itech.sno_api.core.AuthTokenFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.util.Arrays
 
 @EnableWebSecurity
 @Configuration
@@ -35,35 +39,45 @@ open class WebSecurityConfig {
         return BCryptPasswordEncoder()
     }
 
+
+    @Bean
+    open fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:3000") // тут наш домен типо
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        configuration.allowedHeaders = listOf("*")
+        configuration.allowCredentials = true
+        configuration.maxAge = 3600L
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+
     @Bean
     open fun filterChain(http: HttpSecurity): SecurityFilterChain {
         return http
-            .formLogin {
-                it.disable()
+            .cors { cors ->
+                cors.configurationSource(corsConfigurationSource())
             }
             .csrf { it.disable() }
-            .cors { it.disable() }
-            .httpBasic { it.disable() }
-            .authorizeHttpRequests {
-                it.requestMatchers(
-                    "/api/auth/login",
-                    "/api/auth/register",
-                    "/api/**",
-                    // Добавляем пути для Swagger UI
-                    "/swagger-ui/**",  // URL для доступа к Swagger UI
-                    "/v3/api-docs/**", // URL для Swagger API docs (OpenAPI)
-                    "/swagger-ui.html",
-                    "/favicon.ico",
-                    "/webjars/**"     // URL для статических ресурсов, используемых Swagger UI
-
-
-                ).permitAll()
-                it.anyRequest().authenticated()
+            .authorizeHttpRequests { authorize ->
+                authorize
+                    .requestMatchers(
+                        "/api/auth/login",
+                        "/api/auth/register",
+                        "/api/**",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/swagger-ui.html",
+                        "/favicon.ico",
+                        "/webjars/**"
+                    ).permitAll()
+                    .anyRequest().authenticated()
             }
             .exceptionHandling { exception ->
-                exception.authenticationEntryPoint(
-                    provideAuthEntryPoint(),
-                )
+                exception.authenticationEntryPoint(provideAuthEntryPoint())
             }
             .addFilterAfter(provideAuthTokenFilter(), UsernamePasswordAuthenticationFilter::class.java)
             .build()
